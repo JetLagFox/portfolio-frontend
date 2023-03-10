24b04ef9deea5bf9145c818ef4513b35d4552f5e
Egoi Cantero Viqueira <jetlagfox@MacBook-Air-de-Egoi.local>
2023-03-11 00:11:12 +0100

    fix/handle error response

--- components/Experience.js
+++ components/Experience.js
@@ -1,83 +1,89 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
-import ExperienceItem from './../atoms/ExperienceItem';
-import TagItem from '../atoms/TagItem';
+import ExperienceItem from "./../atoms/ExperienceItem";
+import TagItem from "../atoms/TagItem";
 
 const checkFilter = (experience, filters) => {
-	let experienceFiltered = false;
-	filters.map(filter => {
-		if (experience.tags.indexOf(filter) > -1) {
-			experienceFiltered = true;
-		}
-	})
-	return experienceFiltered;
-}
+  let experienceFiltered = false;
+  filters.map((filter) => {
+    if (experience.tags.indexOf(filter) > -1) {
+      experienceFiltered = true;
+    }
+  });
+  return experienceFiltered;
+};
 
-const Experience = ({data}) => {
+const Experience = ({ data }) => {
+  const [filterBy, setFilterBy] = useState([]);
+  const [tagsArray, setTagsArray] = useState([]);
+  const [experiences, setExperiences] = useState();
 
-	const [filterBy, setFilterBy] = useState([]);
-	const [tagsArray, setTagsArray] = useState([]);
-	const [experiences, setExperiences] = useState();
+  useEffect(() => {
+    const customArray = [];
 
-	useEffect(() => {
-		const customArray = [];
-		
-		data?.experiences.map((item) => {
-			item.tags.split(";").map((tag) => {
-				customArray.indexOf(tag) == -1 && customArray.push(tag)
-			})
-		})
+    data?.experiences?.map((item) => {
+      item.tags.split(";").map((tag) => {
+        customArray.indexOf(tag) == -1 && customArray.push(tag);
+      });
+    });
 
-		setTagsArray(customArray);
-		setExperiences(data.experiences);
-	}, [data])
+    setTagsArray(customArray);
+    setExperiences(data.experiences);
+  }, [data]);
 
-	useEffect(() => {
-		if (filterBy?.length > 0) {
-			setExperiences(data.experiences.filter(experience => checkFilter(experience,filterBy))); 
-		} else {
-			setExperiences(data.experiences);
-		}
-	}, [filterBy])
+  useEffect(() => {
+    if (filterBy?.length > 0) {
+      setExperiences(
+        data.experiences.filter((experience) =>
+          checkFilter(experience, filterBy)
+        )
+      );
+    } else {
+      setExperiences(data.experiences);
+    }
+  }, [filterBy]);
 
-	const handleSelection = (item) => {
-		filterBy?.indexOf(item) > -1
-			? setFilterBy(oldArray => oldArray.filter(key => key !== item ))
-			: setFilterBy(oldArray => [...oldArray, item]);
-	}
+  const handleSelection = (item) => {
+    filterBy?.indexOf(item) > -1
+      ? setFilterBy((oldArray) => oldArray.filter((key) => key !== item))
+      : setFilterBy((oldArray) => [...oldArray, item]);
+  };
 
-	return (
-		<section className="experience">
-			<div className="wrapper">
-				<h2>Experiencia Laboral</h2>
-				<p>
-					{
-						tagsArray.map((item, index) => {
-							return <TagItem key={index} tag={item} handleSelection={handleSelection} />
-						})
-					}
-				</p>
-				<ul className='experience__wrap'>
-					{ 
-						experiences?.map((item, index) => {
-							return <ExperienceItem 
-								job={item.job}
-								city={item.city}
-								country={item.country}
-								startDate={item.startDate}
-								finishDate={item.finishDate}
-								description={item.description}
-								company={item.company}
-								key={index}
-								tags={item.tags}
-							/>
-						})
-					}
-				</ul>
-			</div>	
-		</section>
-			
-	)
-}
+  return (
+    <section className="experience">
+      <div className="wrapper">
+        <h2>Experiencia Laboral</h2>
+        <p>
+          {tagsArray.map((item, index) => {
+            return (
+              <TagItem
+                key={index}
+                tag={item}
+                handleSelection={handleSelection}
+              />
+            );
+          })}
+        </p>
+        <ul className="experience__wrap">
+          {experiences?.map((item, index) => {
+            return (
+              <ExperienceItem
+                job={item.job}
+                city={item.city}
+                country={item.country}
+                startDate={item.startDate}
+                finishDate={item.finishDate}
+                description={item.description}
+                company={item.company}
+                key={index}
+                tags={item.tags}
+              />
+            );
+          })}
+        </ul>
+      </div>
+    </section>
+  );
+};
 
 export default Experience;
